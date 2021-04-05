df8f6b29f3d33b521d246bf8ebf14e6557736b44
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-04-05 16:37:08 +0900

    fix(youtube): fix getStartTime

--- src/youtube/getStartTime.ts
+++ src/youtube/getStartTime.ts
@@ -3,17 +3,13 @@ import { youtube_v3 } from 'googleapis'
 export default function getStartTime({
   liveStreamingDetails
 }: youtube_v3.Schema$Video): Date {
-  if (
-    !liveStreamingDetails ||
-    !liveStreamingDetails.actualStartTime ||
-    !liveStreamingDetails.scheduledStartTime
-  ) {
+  const startTime =
+    liveStreamingDetails?.actualStartTime ??
+    liveStreamingDetails?.scheduledStartTime
+
+  if (!startTime) {
     throw new TypeError('Does not include start time.')
   }
-
-  const startTime =
-    liveStreamingDetails.actualStartTime ??
-    liveStreamingDetails.scheduledStartTime
 
   return new Date(startTime)
 }
