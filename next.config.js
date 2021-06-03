17a666cf3f13c43a08b424bc49412690260497e5
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-06-04 00:33:14 +0900

    feat: add redirect from root

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@ module.exports = {
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja']
+  },
+  async redirects() {
+    return [
+      {
+        destination: `https://www.youtube.com/channel/${process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID}`,
+        permanent: false,
+        source: '/'
+      }
+    ]
   }
 }
